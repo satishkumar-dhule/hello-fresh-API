ba68c4ae3de0daf431eb0aa0b40c2fd7bf2867fe
Satishkumar Dhule <satishkumar.dhule@gmail.com>
2021-11-05 19:49:45 +0530

    log for setup and exe permissions

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 virtualenv vnev.hello-fresh
 source vnev.hello-fresh/bin/activate
-pip install -r requirements.txt
+pip install -r requirements.txt >install.log 2>install.error
 ./manage.py compose up -d
